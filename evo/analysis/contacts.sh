cfe4b416076acf31a12349beca9802991fac01de
jamesengleback <james.engleback@mellizyme.com>
2021-11-01 08:07:40 +0000

    docking analysis - saved to a json file

--- evo/analysis/contacts.sh
+++ evo/analysis/contacts.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 DIR="../runs"
-for RUN in $(ls $DIR); do
-	for MUTANT in $(ls $DIR/$RUN);
-		do prody contacts -p $DIR/$MUTANT $DIR/$RUN/$MUTANT/clean* $DIR/$RUN/$MUTANT/mode* ;
+cd ../runs
+for RUN in $(ls); do
+	for MUTANT in $(ls $RUN);
+		do prody contacts -p $MUTANT/$RUN/$MUTANT/clean* $RUN/$MUTANT/mode*  ;
 	done;
 done
+
+wait $(jobs -p)
 
 cat *pdb | grep -v REMARK > ../analysis/all-contacts.pdb
 rm *pdb
