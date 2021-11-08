e7bf6396493f83ab1e4db39bcd2e46a860538538
jamesengleback <james.engleback@mellizyme.com>
2021-11-08 16:58:38 +0000

    ran contacts & metrics gathering scripts on runs/newscore/, organized analysis/

---
+++ evo/analysis/scripts/get-data.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import pandas as pd
+
+def main(args):
+    files = []
+    for root in args:
+        files = files + [i.replace('\n','') for i in os.popen('find ../runs -maxdepth 2 -name scores.csv')]
+    df = pd.concat([pd.read_csv(i) for i in files], axis=0)
+
+    df.to_csv(sys.stdout)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
