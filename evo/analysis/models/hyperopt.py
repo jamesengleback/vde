e29439c011dcc9fbbd8d8977de45926236a57502
jamesengleback <james.engleback@mellizyme.com>
2021-11-09 20:32:50 +0000

    dict mergind using | in hyperopt.py - only works in py 3.9

--- evo/analysis/models/hyperopt.py
+++ evo/analysis/models/hyperopt.py
@@ -10,10 +10,10 @@ import pandas as pd
 
 @dataclass
 class ParamSpace:
-    epochs = [1,2] #[5,10,20,50]
+    epochs = [5,10,20,50]
     batch_size = [8, 16, 32]
     learning_rate = [1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8]
-    dim = [2**i for i in range(3,6)]
+    dim = [2**i for i in range(3,8)]
     downsamples = [2,4,6,8]
     recyles = [2,4,6,8]
 
@@ -33,20 +33,24 @@ def main(num):
 
     output = []
     for params in batch:
-        subprocess = os.popen(f"python train.py -x "\
+        subprocess = os.popen(f"python train.py  "\
                             + f" -e {params['epochs']}"\
                             + f" -b {params['batch_size']}"\
                             + f" -lr {params['learning_rate']}"\
                             + f" -d {params['dim']}"\
                             + f" -y {params['downsamples']}"\
                             + f" -r {params['recyles']}"\
+                            + f" -s"\
+                            + f" -l"\
                             #+ f" 2>/dev/null"\
                             )
         try:
-            metrics = json.loads(subprocess.read()) | {'fail':False}
+            #metrics = json.loads(subprocess.read()) | {'fail':False}
+            metrics = json.loads({**subprocess.read(), **{'fail':False}})
         except:
-            metrics = {'loss_aff':'nan', 'loss_dist':'nan', 'loss_score':'nan'} | {'fail':True}
-        output.append(params | metrics)
+            #metrics = {'loss_aff':'nan', 'loss_dist':'nan', 'loss_score':'nan'} | {'fail':True}
+            metrics = {**{'loss_aff':'nan', 'loss_dist':'nan', 'loss_score':'nan'}, **{'fail':True}}
+        output.append({**params, **metrics})
     df = pd.DataFrame(output)
     df.to_csv(sys.stdout)
 
