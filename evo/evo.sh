f49e014359c80fe33beaa3fced9673573dfc4f74
jamesengleback <james.engleback@mellizyme.com>
2021-11-24 11:29:31 +0000

    merged with cleanup

--- evo/evo.sh
+++ evo/evo.sh
@@ -4,4 +4,5 @@ conda activate enz
 #for _ in $(seq  2); do
 #        python evo.py -p 64 -e 4 -n 100 -s 0.25 1> /dev/null &
 #done
-seq 32 | parallel echo {} ; python evo.py -p 64 -e 8 -n 100 -s 0.25 1> /dev/null 
+#seq 32 | parallel echo {} ; python evo.py -p 64 -e 8 -n 100 -s 0.25 1> /dev/null 
+python evo.py -p 4 -e 1 -n 2 -s 0.25 #1> /dev/null
