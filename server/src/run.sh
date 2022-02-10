5b66551e42994223313eed94093c61ed1a90d4dc
jamesengleback <james@engleback>
2022-02-10 18:07:53 +0000

    flask -> wsgi -> nginx stuff

--- server/src/run.sh
+++ server/src/run.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
+uwsgi -s /tmp/vde.sock --manage-script-name --mount /vde=server:app
+
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate enz
 export FLASK_APP=app.py
-N=4
 python app.py 5001
-#for i in $(seq $N); do
-#	python app.py 500$i &
-#	pids[${i}]=$!
-#done
-#
-#for pid in ${pids[*]}; do
-#    wait $pid
-#done
